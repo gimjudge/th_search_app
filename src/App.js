d50b3f871875d4dddb8a2aba75370ecd95f8f61b
gimjudge <gimjudge@hotmail.com>
2019-01-26 12:30:28 -0800

    Customize Your Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Grid, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Jumbotron>
+          <Grid>
+            <h1>Simple Search App</h1>
+            <p>This is a Simple Search App</p>
+
+            <SearchForm></SearchForm>
+          </Grid>
+        </Jumbotron>
+        <Results />
       </div>
     );
   }
---
+++ src/components/Results.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Grid,
+    ListGroup,
+    ListGroupItem
+} from 'react-bootstrap';
+
+const Results = () => (
+    <Grid>
+        <h2>Results List</h2>
+        <ListGroup>
+            {/*loop from db*/}
+            <ListGroupItem>Item 1</ListGroupItem>
+            <ListGroupItem>Item 2</ListGroupItem>
+            <ListGroupItem>...</ListGroupItem>
+        </ListGroup>;
+    </Grid>
+);
+
+export default Results;
